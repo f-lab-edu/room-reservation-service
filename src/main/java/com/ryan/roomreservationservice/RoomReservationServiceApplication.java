662814161167d890184d0d74c5cec7bb14a120eb
RyanSin <ryansin937@gmail.com>
2024-01-15 14:11:28 +0900

    first commit

---
+++ src/main/java/com/ryan/roomreservationservice/RoomReservationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.ryan.roomreservationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoomReservationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoomReservationServiceApplication.class, args);
+	}
+
+}
