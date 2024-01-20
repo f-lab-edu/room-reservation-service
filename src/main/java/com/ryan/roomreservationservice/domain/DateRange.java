4d7d4689f436fb0be1109333097d6116454b8ad6
Ryan-Sin <ryansin937@gmail.com>
2024-01-20 21:24:42 +0900

    fix: 코드 잘못 제거 수정

--- src/main/java/com/ryan/roomreservationservice/domain/DateRange.java
+++ src/main/java/com/ryan/roomreservationservice/domain/DateRange.java
@@ -16,8 +16,8 @@ public class DateRange {
             throw new IllegalArgumentException(ErrorMessage.CANNOT_BE_NULL_VALUE);
         }
 
-        if (Instant.now().isAfter(start) || Instant.now().isAfter(end)) {
-            throw new IllegalArgumentException(ErrorMessage.CANNOT_BE_SET_TO_PAST_DATE);
+        if(start.isAfter(end)){
+            throw new IllegalArgumentException(ErrorMessage.CANNOT_BE_EARLIER_THAN_THE_START_DATE);
         }
 
         this.start = start;
