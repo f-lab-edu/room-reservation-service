36676ae5b2868eeaec9c02633df958eb83c4aa29
Ryan-Sin <ryansin937@gmail.com>
2024-02-10 21:04:14 +0900

    feat: 객실예약 테스트 코드 수정 및 추가

--- src/test/java/com/ryan/roomreservationservice/repository/RoomReservationRepositoryTest.java
+++ src/test/java/com/ryan/roomreservationservice/repository/RoomReservationRepositoryTest.java
@@ -8,11 +8,12 @@ import com.ryan.roomreservationservice.persistance.RoomReservationJpaRepository;
 import com.ryan.roomreservationservice.util.enums.ReservationStatus;
 import com.ryan.roomreservationservice.util.enums.RoomStatus;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.orm.jpa.JpaSystemException;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
@@ -35,7 +36,7 @@ class RoomReservationRepositoryTest {
 
     @BeforeEach
     public void setup() {
-        this.roomJpaRepository.save(
+        roomJpaRepository.save(
                 Room.builder()
                         .roomId(1L)
                         .zoneCode("Asia/Seoul")
@@ -46,54 +47,191 @@ class RoomReservationRepositoryTest {
         );
     }
 
-    @Test
-    public void 객실예약하기() {
-        // given(준비): 어떠한 데이터가 준비되었을 때
-        Room room = this.roomJpaRepository.findByRoomName("그린룸").orElseThrow();
+    @Nested
+    class 객실예약 {
+        @Test
+        public void 단일객실예약() {
+            // given(준비): 어떠한 데이터가 준비되었을 때
+            Room room = roomJpaRepository.findByRoomName("그린룸").orElseThrow();
 
-        Instant reservationStartDate = Instant.parse("2024-02-03T11:00:00.000Z");
-        Instant reservationEndDate = Instant.parse("2024-02-05T11:00:00.000Z");
+            Instant reservationStartDate = Instant.parse("2024-03-03T15:00:00.000Z");
+            Instant reservationEndDate = Instant.parse("2024-03-05T11:00:00.000Z");
 
-        // when(실행): 어떠한 함수를 실행하면
-        RoomReservation roomReservation = RoomReservation.builder()
-                .room(room)
-                .reservationStatus(ReservationStatus.PENDING)
-                .reservation(new DateRange(reservationStartDate, reservationEndDate))
-                .build();
+            // when(실행): 어떠한 함수를 실행하면
+            RoomReservation roomReservation = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate, reservationEndDate))
+                    .build();
 
-        RoomReservation save = this.roomReservationJpaRepository.save(roomReservation);
-        Optional<RoomReservation> findRoomReservation = this.roomReservationJpaRepository.findById(save.getRoomReservationId());
+            RoomReservation save = roomReservationJpaRepository.save(roomReservation);
+            Optional<RoomReservation> findRoomReservation = roomReservationJpaRepository.findById(save.getRoomReservationId());
 
-        // then(검증): 어떠한 결과가 나와야 한다.
-        assertThat(findRoomReservation.get().getRoomReservationId()).isEqualTo(roomReservation.getRoomReservationId());
+            // then(검증): 어떠한 결과가 나와야 한다.
+            assertThat(findRoomReservation.get().getRoomReservationId()).isEqualTo(roomReservation.getRoomReservationId());
+        }
+
+        @Test
+        public void 예약된_객실_퇴실시간의_맞추서_예약() {
+            // given(준비): 어떠한 데이터가 준비되었을 때
+            Room room = roomJpaRepository.findByRoomName("그린룸").orElseThrow();
+
+            Instant reservationStartDate1 = Instant.parse("2024-03-03T15:00:00.000Z");
+            Instant reservationEndDate1 = Instant.parse("2024-03-05T11:00:00.000Z");
+
+            Instant reservationStartDate2 = Instant.parse("2024-03-05T15:00:00.000Z");
+            Instant reservationEndDate2 = Instant.parse("2024-03-08T11:00:00.000Z");
+
+            // when(실행): 어떠한 함수를 실행하면
+            RoomReservation roomReservation1 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate1, reservationEndDate1))
+                    .build();
+
+            RoomReservation roomReservation2 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate2, reservationEndDate2))
+                    .build();
+
+            RoomReservation saved1 = roomReservationJpaRepository.save(roomReservation1);
+            RoomReservation saved2 = roomReservationJpaRepository.save(roomReservation2);
+
+            Optional<RoomReservation> findRoomReservation1 = roomReservationJpaRepository.findById(saved1.getRoomReservationId());
+            Optional<RoomReservation> findRoomReservation2 = roomReservationJpaRepository.findById(saved2.getRoomReservationId());
+
+            // then(검증): 어떠한 결과가 나와야 한다.
+            assertThat(findRoomReservation1.get().getRoomReservationId()).isEqualTo(roomReservation1.getRoomReservationId());
+            assertThat(findRoomReservation2.get().getRoomReservationId()).isEqualTo(roomReservation2.getRoomReservationId());
+        }
     }
 
-    @Test
-    public void 동일한_날짜의_중복객실_예약하기() {
-        // given(준비): 어떠한 데이터가 준비되었을 때
-        Room room = this.roomJpaRepository.findByRoomName("그린룸").orElseThrow();
+    @Nested
+    class 중첩객실예약 {
+        @Test
+        public void 동일한_날짜의_중복객실_예약하기() {
+            // given(준비): 어떠한 데이터가 준비되었을 때
+            Room room = roomJpaRepository.findByRoomName("그린룸").orElseThrow();
 
-        Instant reservationStartDate = Instant.parse("2024-02-03T11:00:00.000Z");
-        Instant reservationEndDate = Instant.parse("2024-02-05T11:00:00.000Z");
+            Instant reservationStartDate = Instant.parse("2024-03-03T15:00:00.000Z");
+            Instant reservationEndDate = Instant.parse("2024-03-05T11:00:00.000Z");
 
-        // when(실행): 어떠한 함수를 실행하면
-        RoomReservation roomReservation1 = RoomReservation.builder()
-                .room(room)
-                .reservationStatus(ReservationStatus.PENDING)
-                .reservation(new DateRange(reservationStartDate, reservationEndDate))
-                .build();
+            // when(실행): 어떠한 함수를 실행하면
+            RoomReservation roomReservation1 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate, reservationEndDate))
+                    .build();
 
-        RoomReservation roomReservation2 = RoomReservation.builder()
-                .room(room)
-                .reservationStatus(ReservationStatus.PENDING)
-                .reservation(new DateRange(reservationStartDate, reservationEndDate))
-                .build();
+            RoomReservation roomReservation2 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate, reservationEndDate))
+                    .build();
 
-        this.roomReservationJpaRepository.save(roomReservation1);
+            roomReservationJpaRepository.save(roomReservation1);
 
-        // then(검증): 어떠한 결과가 나와야 한다.
-        assertThrows(DataIntegrityViolationException.class, () ->
-                this.roomReservationJpaRepository.save(roomReservation2)
-        );
+            // then(검증): 어떠한 결과가 나와야 한다.
+            assertThrows(JpaSystemException.class, () ->
+                    roomReservationJpaRepository.save(roomReservation2)
+            );
+        }
+
+        @Test
+        public void 예약종료_날짜가_중첩되는_상황() {
+            // given(준비): 어떠한 데이터가 준비되었을 때
+            Room room = roomJpaRepository.findByRoomName("그린룸").orElseThrow();
+
+            Instant reservationStartDate1 = Instant.parse("2024-03-10T15:00:00.000Z");
+            Instant reservationEndDate1 = Instant.parse("2024-03-15T11:00:00.000Z");
+
+            // when(실행): 어떠한 함수를 실행하면
+            RoomReservation roomReservation1 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate1, reservationEndDate1))
+                    .build();
+
+            Instant reservationStartDate2 = Instant.parse("2024-03-08T15:00:00.000Z");
+            Instant reservationEndDate2 = Instant.parse("2024-03-13T11:00:00.000Z");
+
+            RoomReservation roomReservation2 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate2, reservationEndDate2))
+                    .build();
+
+            roomReservationJpaRepository.save(roomReservation1);
+
+            // then(검증): 어떠한 결과가 나와야 한다.
+            assertThrows(JpaSystemException.class, () ->
+                    roomReservationJpaRepository.save(roomReservation2)
+            );
+        }
+
+        @Test
+        public void 예약시작_날짜가_중첩되는_상황() {
+            // given(준비): 어떠한 데이터가 준비되었을 때
+            Room room = roomJpaRepository.findByRoomName("그린룸").orElseThrow();
+
+            Instant reservationStartDate1 = Instant.parse("2024-03-10T15:00:00.000Z");
+            Instant reservationEndDate1 = Instant.parse("2024-03-15T11:00:00.000Z");
+
+            // when(실행): 어떠한 함수를 실행하면
+            RoomReservation roomReservation1 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate1, reservationEndDate1))
+                    .build();
+
+            Instant reservationStartDate2 = Instant.parse("2024-03-12T15:00:00.000Z");
+            Instant reservationEndDate2 = Instant.parse("2024-03-18T11:00:00.000Z");
+
+            RoomReservation roomReservation2 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate2, reservationEndDate2))
+                    .build();
+
+            roomReservationJpaRepository.save(roomReservation1);
+
+            // then(검증): 어떠한 결과가 나와야 한다.
+            assertThrows(JpaSystemException.class, () ->
+                    roomReservationJpaRepository.save(roomReservation2)
+            );
+        }
+
+        @Test
+        public void 예약된_날짜가_포함되는_상황() {
+            // given(준비): 어떠한 데이터가 준비되었을 때
+            Room room = roomJpaRepository.findByRoomName("그린룸").orElseThrow();
+
+            Instant reservationStartDate1 = Instant.parse("2024-03-10T15:00:00.000Z");
+            Instant reservationEndDate1 = Instant.parse("2024-03-15T11:00:00.000Z");
+
+            // when(실행): 어떠한 함수를 실행하면
+            RoomReservation roomReservation1 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate1, reservationEndDate1))
+                    .build();
+
+            Instant reservationStartDate2 = Instant.parse("2024-03-08T15:00:00.000Z");
+            Instant reservationEndDate2 = Instant.parse("2024-03-18T11:00:00.000Z");
+
+            RoomReservation roomReservation2 = RoomReservation.builder()
+                    .room(room)
+                    .reservationStatus(ReservationStatus.PENDING)
+                    .reservation(new DateRange(reservationStartDate2, reservationEndDate2))
+                    .build();
+
+            roomReservationJpaRepository.save(roomReservation1);
+
+            // then(검증): 어떠한 결과가 나와야 한다.
+            assertThrows(JpaSystemException.class, () ->
+                    roomReservationJpaRepository.save(roomReservation2)
+            );
+        }
     }
 }
