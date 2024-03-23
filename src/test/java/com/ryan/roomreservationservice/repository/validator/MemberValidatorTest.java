4f0b92a32f4d78f3aefc64f324e05682d7c3d030
Ryan-Sin <ryansin937@gmail.com>
2024-03-24 03:47:01 +0900

    feat: 로그인 API 1차 완료

--- src/test/java/com/ryan/roomreservationservice/repository/validator/MemberValidatorTest.java
+++ src/test/java/com/ryan/roomreservationservice/repository/validator/MemberValidatorTest.java
@@ -4,9 +4,12 @@ import com.ryan.roomreservationservice.domain.Member;
 import com.ryan.roomreservationservice.util.exception.CommonException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.Optional;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -16,7 +19,7 @@ class MemberValidatorTest {
 
     @BeforeEach()
     void init() {
-        this.memberValidator = new MemberValidator();
+        this.memberValidator = new MemberValidator(new BCryptPasswordEncoder());
     }
 
     @Test
@@ -41,5 +44,34 @@ class MemberValidatorTest {
         // when(실행): 어떠한 함수를 실행하면, then(검증): 어떠한 결과가 나와야 한다.
         assertThatCode(() -> memberValidator.assertMemberExist(nullMember))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    public void 비밀번호_매칭_성공() {
+        // given(준비): 어떠한 데이터가 준비되었을 때
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String password = "1234qweR!!";
+
+        // when(실행): 어떠한 함수를 실행하면
+        String encryptionPassword = passwordEncoder.encode(password);
+        boolean matchesPassword = passwordEncoder.matches(password, encryptionPassword);
+
+        // then(검증): 어떠한 결과가 나와야 한다.
+        assertThat(matchesPassword).isTrue();
+    }
+
+    @Test
+    public void 비밀번호_매칭_실패() {
+        // given(준비): 어떠한 데이터가 준비되었을 때
+        PasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+        String password = "1234qweR!!";
+        String encoderPassword = "1q2w3e4R!!";
+
+        // when(실행): 어떠한 함수를 실행하면
+        String encryptionPassword = passwordEncoder.encode(password);
+        boolean matchesPassword = passwordEncoder.matches(encoderPassword, encryptionPassword);
+
+        // then(검증): 어떠한 결과가 나와야 한다.
+        assertThat(matchesPassword).isFalse();
     }
 }
