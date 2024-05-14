a000212980c56292dbd397202053e359156f0131
Ryan-Sin <ryansin937@gmail.com>
2024-05-15 00:39:03 +0900

    fix: DateRange 생성 로직 및 날짜 기간 로직 변경

--- src/main/java/com/ryan/roomreservationservice/domain/DateRange.java
+++ src/main/java/com/ryan/roomreservationservice/domain/DateRange.java
@@ -4,36 +4,25 @@ import com.ryan.roomreservationservice.utils.exception.ErrorMessage;
 
 import java.time.Duration;
 import java.time.Instant;
+import java.time.ZoneId;
+import java.time.temporal.ChronoUnit;
 import java.util.Objects;
 
 public record DateRange(Instant start, Instant end) {
-    public DateRange {
+    public DateRange(Instant start, Instant end) {
         if (Objects.isNull(start) || Objects.isNull(end)) {
             throw new IllegalArgumentException(ErrorMessage.CANNOT_BE_NULL_VALUE);
-        }
-
-        if (this.extractHour(start) != 15 || this.extractHour(end) != 11) {
-            throw new IllegalArgumentException(ErrorMessage.CHECK_AVAILABLE_TIME_FOR_RESERVATION);
         }
 
         if (start.isAfter(end)) {
             throw new IllegalArgumentException(ErrorMessage.CANNOT_BE_EARLIER_THAN_THE_START_DATE);
         }
+
+        this.start = start.truncatedTo(ChronoUnit.DAYS);
+        this.end = end.truncatedTo(ChronoUnit.DAYS);
     }
 
-    public long calculateDayPeriod() {
-        long hours = Duration.between(this.start, this.end).toHours();
-
-        if (hours == 20) {
-            return 1;
-        }
-
-        hours -= 20;
-        return (hours / 24) + 1;
-    }
-
-    private long extractHour(Instant instant) {
-        long epochSecond = instant.getEpochSecond();
-        return (epochSecond % (60 * 60 * 24)) / (60 * 60);
+    public long calculateDayPeriod(ZoneId zoneId) {
+        return Duration.between(this.start.atZone(zoneId), this.end.atZone(zoneId)).toDays();
     }
 }
--- src/test/java/com/ryan/roomreservationservice/domain/DateRangeTest.java
+++ src/test/java/com/ryan/roomreservationservice/domain/DateRangeTest.java
@@ -1,14 +1,22 @@
 package com.ryan.roomreservationservice.domain;
 
 import com.ryan.roomreservationservice.utils.exception.ErrorMessage;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.time.Instant;
+import java.time.ZoneId;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 class DateRangeTest {
+    ZoneId zoneId;
+
+    @BeforeEach
+    public void init() {
+        this.zoneId = ZoneId.of("Asia/Seoul");
+    }
 
     @Test
     public void 올바른_날짜_생성() {
@@ -58,7 +66,7 @@ class DateRangeTest {
         DateRange dateRange = new DateRange(start, end);
 
         // when(실행): 어떠한 함수를 실행하면
-        long day = dateRange.calculateDayPeriod();
+        long day = dateRange.calculateDayPeriod(zoneId);
 
         // then(검증): 어떠한 결과가 나와야 한다.
         assertThat(day).isEqualTo(1);
@@ -73,7 +81,7 @@ class DateRangeTest {
         DateRange dateRange = new DateRange(start, end);
 
         // when(실행): 어떠한 함수를 실행하면
-        long day = dateRange.calculateDayPeriod();
+        long day = dateRange.calculateDayPeriod(zoneId);
 
         // then(검증): 어떠한 결과가 나와야 한다.
         assertThat(day).isEqualTo(2);
@@ -88,36 +96,10 @@ class DateRangeTest {
         DateRange dateRange = new DateRange(start, end);
 
         // when(실행): 어떠한 함수를 실행하면
-        long day = dateRange.calculateDayPeriod();
+        long day = dateRange.calculateDayPeriod(zoneId);
 
         // then(검증): 어떠한 결과가 나와야 한다.
         assertThat(day).isEqualTo(30);
-    }
-
-    @Test
-    public void 시작_시간이_잘못된_경우_검증() {
-        // given(준비): 어떠한 데이터가 준비되었을 때
-        Instant start = Instant.parse("2024-02-01T16:00:00.000Z");
-        Instant end = Instant.parse("2024-03-02T11:00:00.000Z");
-
-        // when(실행): 어떠한 함수를 실행하면
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new DateRange(start, end));
-
-        // then(검증): 어떠한 결과가 나와야 한다.
-        assertThat(exception.getMessage()).isEqualTo(ErrorMessage.CHECK_AVAILABLE_TIME_FOR_RESERVATION);
-    }
-
-    @Test
-    public void 종료_시간이_잘못된_경우_검증() {
-        // given(준비): 어떠한 데이터가 준비되었을 때
-        Instant start = Instant.parse("2024-02-01T15:00:00.000Z");
-        Instant end = Instant.parse("2024-03-02T12:00:00.000Z");
-
-        // when(실행): 어떠한 함수를 실행하면
-        IllegalArgumentException exception = assertThrows(IllegalArgumentException.class, () -> new DateRange(start, end));
-
-        // then(검증): 어떠한 결과가 나와야 한다.
-        assertThat(exception.getMessage()).isEqualTo(ErrorMessage.CHECK_AVAILABLE_TIME_FOR_RESERVATION);
     }
 
 }
