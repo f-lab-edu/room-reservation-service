eb093dc6a1eda094861bf766e49181610267edeb
Ryan-Sin <ryansin937@gmail.com>
2024-05-24 18:58:40 +0900

    fix: 리팩토링 진행

--- src/main/java/com/ryan/roomreservationservice/domain/Accommodation.java
+++ src/main/java/com/ryan/roomreservationservice/domain/Accommodation.java
@@ -43,20 +43,22 @@ public class Accommodation {
     }
 
     public BigDecimal calculateRoomRefundAmount(LocalDate cancelLocalDate, LocalDateRange reservationDate) {
-        LocalDate start = reservationDate.start();
+        reservationDate.assertDateBeforeTheStart(cancelLocalDate);
 
-        if (start.isBefore(cancelLocalDate))
-            throw new IllegalArgumentException(ErrorMessage.CANCEL_REQUEST_DATE_MUST_BE_BEFORE_CHECK_IN);
-
-        long beforeDay = reservationDate.calculatePeriodBeforeStartDate(cancelLocalDate);
-        if (3 < beforeDay && beforeDay < 7) {
-            BigDecimal refundRate = BigDecimal.valueOf(70).divide(BigDecimal.valueOf(100));
+        BigDecimal refundRate = calculateRefundRate(cancelLocalDate, reservationDate);
+        if(refundRate.compareTo(BigDecimal.ZERO) > 0) {
             BigDecimal refundAmount = this.price.multiply(refundRate);
-
             return refundAmount.setScale(0, RoundingMode.HALF_UP);
         }
-
         return this.price;
+    }
+
+    private static BigDecimal calculateRefundRate(LocalDate cancelLocalDate, LocalDateRange reservationDate) {
+        long beforeDay = reservationDate.calculatePeriodBeforeStartDate(cancelLocalDate);
+        if (3 < beforeDay && beforeDay < 7) {
+            return BigDecimal.valueOf(70).divide(BigDecimal.valueOf(100));
+        }
+        return BigDecimal.ZERO;
     }
 
     private void pendingReservation(Accommodation accommodation) {
