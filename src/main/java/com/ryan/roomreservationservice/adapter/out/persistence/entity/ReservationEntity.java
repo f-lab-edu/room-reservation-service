ec9c7fcd398f4443003208ae2722c2cd2460cfe8
Ryan-Sin <ryansin937@gmail.com>
2024-06-04 03:51:36 +0900

    fix: 잘못된 import 수정

--- src/main/java/com/ryan/roomreservationservice/adapter/out/persistence/entity/ReservationEntity.java
+++ src/main/java/com/ryan/roomreservationservice/adapter/out/persistence/entity/ReservationEntity.java
@@ -1,6 +1,6 @@
 package com.ryan.roomreservationservice.adapter.out.persistence.entity;
 
-import com.ryan.roomreservationservice.domain.LocalDateRange;
+import com.ryan.roomreservationservice.domain.record.LocalDateRange;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
