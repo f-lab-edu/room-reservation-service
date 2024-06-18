76e8925e92547e6b7c33a4f40e5ddc764eb0b953
Ryan-Sin <ryansin937@gmail.com>
2024-06-19 01:10:02 +0900

    fix: 의존성 주입 컴파일 에러 수정

--- src/main/java/com/ryan/roomreservationservice/adapter/out/persistence/RoomPersistenceAdapter.java
+++ src/main/java/com/ryan/roomreservationservice/adapter/out/persistence/RoomPersistenceAdapter.java
@@ -1,18 +1,24 @@
 package com.ryan.roomreservationservice.adapter.out.persistence;
 
-import com.ryan.roomreservationservice.adapter.out.persistence.entity.RoomEntity;
 import com.ryan.roomreservationservice.adapter.out.persistence.entity.RoomJpaRepository;
+import com.ryan.roomreservationservice.adapter.out.persistence.mapper.RoomPersistenceMapper;
+import com.ryan.roomreservationservice.application.port.out.QueryRoomPort;
+import com.ryan.roomreservationservice.domain.Room;
 import com.ryan.roomreservationservice.utils.exception.ErrorMessage;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-class RoomPersistenceAdapter {
+class RoomPersistenceAdapter implements QueryRoomPort {
     private final RoomJpaRepository roomJpaRepository;
+    private final RoomPersistenceMapper roomPersistenceMapper;
 
-    public RoomEntity findOneRoomByName(String name) {
+
+    @Override
+    public Room findOneByName(String name) {
         return this.roomJpaRepository.findByName(name)
+                .map(roomPersistenceMapper::mapToRoom)
                 .orElseThrow(() -> new IllegalArgumentException(ErrorMessage.NOT_FOUNT_ROOM));
     }
 }
